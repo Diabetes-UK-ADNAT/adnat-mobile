3bebf89d27e33b4df588c1620464aa199d4b31f8
Eric Link <eric@ericmlink.com>
2012-12-28 12:54:01 -0600

    Cleaned up question code

--- app/lib/question.js
+++ app/lib/question.js
@@ -6,53 +6,52 @@ function setTitle(component,q) {
 	html = tpl.apply( {t: q.get('text') } );
 	component.getTitle().setHtml(html);
 }
-function setQuestion(f, q) {
-	// set question
-	f.removeAll();
+function setQuestion(c, q) {
+	c.removeAll();
 	var response = buildQuestion(q);
-	f.add(Ext.decode(response).data);
+	c.add(Ext.decode(response).data);
 }
 
 function buildQuestion(q) {
 	//fixme switch on question types here
-		var tpl = new Ext.XTemplate(
-			'{ success: true, data: [ ' ,
-				// fields
-				'{ xtype: "hiddenfield", name: "ordinal", suborgetsdropped: "{name}"}, ' ,
-				// options fieldset
-				'{ xtype: "fieldset", items: [ ',
-					'<tpl for="options">',     
-					'{ ' ,
-						'xtype: "radiofield", ' ,
-						'name : "color", ' ,
-						'label: "{text}", ' ,
-						'labelWidth: "66%",	 ' ,
-						'listeners: { ',
-							'check: function(button) { ',
-								'Ext.get("{parent.name}").setHtml("' ,
-									"<div id='{parent.name}' class='centered'>",
-										"<img src='../resources/images/tl-{color}.png'/>",
-									"</div>" ,
-								'"); ',
-							'}',
-						'}, ' ,
+	var tpl = new Ext.XTemplate(
+		'{ success: true, data: [ ' ,
+			// fields
+			'{ xtype: "hiddenfield", name: "ordinal", suborgetsdropped: "{name}"}, ' ,
+			// options fieldset
+			'{ xtype: "fieldset", items: [ ',
+				'<tpl for="options">',     
+				'{ ' ,
+					'xtype: "radiofield", ' ,
+					'name : "color", ' ,
+					'label: "{text}", ' ,
+					'labelWidth: "66%",	 ' ,
+					'listeners: { ',
+						'check: function(button) { ',
+							'Ext.get("{parent.name}").setHtml("' ,
+								"<div id='{parent.name}' class='centered'>",
+									"<img src='../resources/images/tl-{color}.png'/>",
+								"</div>" ,
+							'"); ',
+						'}',
 					'}, ' ,
-					'</tpl>',
-				'], }, ' , 
-				// stoplight
-				"{ html: '",
-				'<div>&nbsp;</div>',
-				'<div id="{name}" class="centered">',
-					'<img src="../resources/images/tl-none.png"/>',
-				'</div>',
-				'<div>&nbsp;</div>',
-				"'}, ",
-			'] }'  
-		);
-		tpl.compile();
-		var response = tpl.apply({name:Date.now(), options:q.get('options')});  
-		//console.log(response);
-		return response;
+				'}, ' ,
+				'</tpl>',
+			'], }, ' , 
+			// stoplight
+			"{ html: '",
+			'<div>&nbsp;</div>',
+			'<div id="{name}" class="centered">',
+				'<img src="../resources/images/tl-none.png"/>',
+			'</div>',
+			'<div>&nbsp;</div>',
+			"'}, ",
+		'] }'  
+	);
+	tpl.compile();
+	var response = tpl.apply({name:Date.now(), options:q.get('options')});  
+	//console.log(response);
+	return response;
 }	
 
 
