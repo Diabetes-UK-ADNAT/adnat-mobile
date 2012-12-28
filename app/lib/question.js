e28076429c0a302410431a55e66105eceac64b05
Eric Link <eric@ericmlink.com>
2012-12-28 12:22:43 -0600

    Stoplight question from template with options from question.js - hidden field stripped

--- app/lib/question.js
+++ app/lib/question.js
@@ -1,62 +1,50 @@
 function setTitle(component,q) {
-	var t = new Ext.Template([
+	var tpl = new Ext.Template([
 		'<div class="question">{t}</div>',
 	]);
-	t.compile();
-	html = t.apply( {t: q.get('text') } );
+	tpl.compile();
+	html = tpl.apply( {t: q.get('text') } );
 	component.getTitle().setHtml(html);
 }
 
-function getStopLight(name) {
-	var t = new Ext.Template([
-	"{ html: '",
-	'<div>&nbsp;</div>',
-	'<div id={n} class="centered">',
-		'<img src="../resources/images/tl-none.png"/>',
-	'</div>',
-	'<div>&nbsp;</div>',
-    "'}, ",
-	]);
-	t.compile();
-	return t.apply( {n: name } );
-}
-function getStopLightOption(name,text,color) {
-	var t = new Ext.Template([
-	'{ ' ,
-		'xtype: "radiofield", ' ,
-		'name : "color", ' ,
-		'label: "{txt}", ' ,
-		'labelWidth: "66%",	 ' ,
-		'listeners: { ',
-			'check: function(button) { ',
-				'Ext.get("{n}").setHtml("' ,
-					"<div id='{n}' class='centered'>",
-						"<img src='../resources/images/tl-{c}.png'/>",
-					"</div>" ,
-				'"); ',
-			'}',
-	    '}, ' ,
-	'}, ' ,
-	]);
-	t.compile();
-	return t.apply( {txt: text, n: name, c:color } );
-}	
-
 function buildQuestion(q) {
-		var name = Date.now();
-		var options = q.get('options');
-		var response = '{ success: true, data: [ ' +
-			'{xtype: "hiddenfield", name: "ordinal",}, ' +
-			'{ xtype: "fieldset", items: [ ';
-
-			//for each option (fixme: could do w/ template also)
-			options.forEach(function(entry) {
-				response = response + getStopLightOption(name,entry['text'],entry['color']);
-			});
-			//
-			response = response + 
-				'], }, ' + //items
-				getStopLight(name) + 
-				'] }' ; //outer json array
+		var nm = Date.now();
+		var opts = q.get('options');
+		var tpl = new Ext.XTemplate(
+			'{ success: true, data: [ ' ,
+				//fixme stripped by template? '{ xtype: "hiddenfield", name: "ordinal", }, ' ,
+				'{ xtype: "fieldset", items: [ ',
+					'<tpl for="options">',     
+					'{ ' ,
+						'xtype: "radiofield", ' ,
+						'name : "color", ' ,
+						'label: "{text}", ' ,
+						'labelWidth: "66%",	 ' ,
+						'listeners: { ',
+							'check: function(button) { ',
+								'Ext.get("{parent.name}").setHtml("' ,
+									"<div id='{parent.name}' class='centered'>",
+										"<img src='../resources/images/tl-{color}.png'/>",
+									"</div>" ,
+								'"); ',
+							'}',
+						'}, ' ,
+					'}, ' ,
+					'</tpl>',
+				'], }, ' , 
+				"{ html: '",
+				'<div>&nbsp;</div>',
+				'<div id="{name}" class="centered">',
+					'<img src="../resources/images/tl-none.png"/>',
+				'</div>',
+				'<div>&nbsp;</div>',
+				"'}, ",
+			'] }'  
+		);
+		tpl.compile();
+		var response = tpl.apply({name:nm, options:opts});  
+		console.log(response);
 		return response;
 }	
+
+
