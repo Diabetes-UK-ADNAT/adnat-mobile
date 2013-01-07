0e4bd91d1006098e3c6947e24e658a0df628f3c6
Eric Link <eric@ericmlink.com>
2013-01-06 20:08:27 -0600

    Array utils

--- app/lib/util.js
+++ app/lib/util.js
@@ -2,6 +2,18 @@ function log(m) {
 	console.log(m);
 }
 
+function makeArray(v) {
+	if (v != null ) {
+		return v instanceof Array ? v : new Array(v);
+	}
+	return new Array();
+}
+
+function logArray(arr) {
+	if (arr != null) arr.forEach(logArrayElements);
+}
+
+
 function logArrayElements(element, index, array) {
     log("a[" + index + "] = " + element);
 }
