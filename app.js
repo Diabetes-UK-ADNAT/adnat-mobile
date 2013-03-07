30f24913d66098f4568e7d314c881edbb826d9a6
Eric Link <eric@ericmlink.com>
2013-03-06 23:26:37 -0600

    Offline question store working

--- app.js
+++ app.js
@@ -7,48 +7,42 @@ Ext.Loader.setPath({
 
 Ext.application({
     name: 'adnat',
-
     requires: [
         'Ext.MessageBox'
     ],
-
     models: [
-		'Question',
-		'Response',
-		'ScoreConfig'
+        'Question',
+        'QuestionOnline',
+        'Response',
+        'ScoreConfig'
     ],
-
     views: [
-		'Main', 
-		'Welcome', 
-		'Help', 
-		'Contact', 
-		'Results', 
-		'Question', 
-		'Util', 
-		'ServerUnavailable'
-	],
-
-	controllers: [
+        'Main',
+        'Welcome',
+        'Help',
+        'Contact',
+        'Results',
+        'Question',
+        'Util',
+        'ServerUnavailable'
+    ],
+    controllers: [
         'QuestionController',
         'ResultsController'
     ],
-
     stores: [
-		'Questions',
-		'Responses',
-		'ScoreConfigs'
-	],
-
+        'Questions',
+        'QuestionsOnline',
+        'Responses',
+        'ScoreConfigs'
+    ],
     icon: {
         '57': 'resources/icons/Icon.png',
         '72': 'resources/icons/Icon~ipad.png',
         '114': 'resources/icons/Icon@2x.png',
         '144': 'resources/icons/Icon~ipad@2x.png'
     },
-
     isIconPrecomposed: true,
-
     startupImage: {
         '320x460': 'resources/startup/320x460.jpg',
         '640x920': 'resources/startup/640x920.png',
@@ -57,7 +51,6 @@ Ext.application({
         '1536x2008': 'resources/startup/1536x2008.png',
         '1496x2048': 'resources/startup/1496x2048.png'
     },
-
     launch: function() {
         // Destroy the #appLoadingIndicator element
         Ext.fly('appLoadingIndicator').destroy();
@@ -65,18 +58,19 @@ Ext.application({
         // Initialize the main view
         Ext.Viewport.add(Ext.create('adnat.view.Main'));
 
-		// fixme go to questions
-		// Ext.getCmp('mainTabPanel').setActiveItem(1);
+        // fixme go to questions
+        // Ext.getCmp('mainTabPanel').setActiveItem(1);
+
     },
     onUpdated: function() {
         Ext.Msg.confirm(
-            "Application Update",
-            "This application has just successfully been updated to the latest version. Reload now?",
-            function(buttonId) {
-                if (buttonId === 'yes') {
-                    window.location.reload();
+                "Application Update",
+                "This application has just successfully been updated to the latest version. Reload now?",
+                function(buttonId) {
+                    if (buttonId === 'yes') {
+                        window.location.reload();
+                    }
                 }
-            }
         );
     }
 });
