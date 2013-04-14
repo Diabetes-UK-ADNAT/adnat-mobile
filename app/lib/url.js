cd6da3a5fbe1dfc0c80f7d62518380d146ff3dbb
Eric Link <eric@ericmlink.com>
2013-04-14 10:37:44 -0500

    prod url config

--- app/lib/url.js
+++ app/lib/url.js
@@ -1,5 +1,5 @@
 var AppUrl = {
-    isDev: true,
+    isDev: false,
     'assessments': function() {
         return this.isDev ? 'https://api.myadnat.co.uk:4443/v1/assessments' : 'https://api.myadnat.co.uk/v1/assessments';
     }
