0a5e0e43eb096f4993855b9b150e2fc0f0291cd0
Eric Link <eric@ericmlink.com>
2013-05-24 23:52:30 -0500

    config urls

--- prepush.sh
+++ prepush.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-date
-time find ./build/adnat/production/ -type f -name "*.js" |xargs grep "isDev: false"
-date
-
-echo "Look for isDev: false and you are ready to go"
+find . -type f -name "*.js" |xargs grep --color "isDev:"
